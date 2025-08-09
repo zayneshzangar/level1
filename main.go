c9dc30d09d5be3a9b2012d21a52ac5f51484b117
zayneshzangar <zangarzaynesh@gmail.com>
2025-08-09 20:11:00 +0500

    .

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	a.SetName("Zangar")
 	a.SetAge(31)
 	
-	fmt.Printf("My name is %s and I'm %d years old!\n",a.GetName(), a.GetAge())
+	fmt.Printf("My name is %s and I'm %d years old!\n", a.GetName(), a.GetAge())
 	fmt.Println("END level 1.1")
 	fmt.Println("----------------------------------------")
 
