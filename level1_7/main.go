c75ac5548be2dbb5f5ad1392fd619b0c9df67399
zayneshzangar <zangarzaynesh@gmail.com>
2025-08-11 21:42:01 +0500

    Конкурентная запись в map

---
+++ level1_7/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type SafeMap struct {
+	mu sync.Mutex
+	m  map[string]int
+}
+
+func NewSafeMap() *SafeMap {
+	return &SafeMap{
+		m: make(map[string]int),
+	}
+}
+
+func (s *SafeMap) Set(key string, value int) {
+	s.mu.Lock()
+	s.m[key] = value
+	s.mu.Unlock()
+}
+
+func main() {
+	safeMap := NewSafeMap()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000000; j++ {
+				safeMap.Set("test-key", j)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	// Читаем значения
+	for k, v := range safeMap.m {
+		fmt.Println(k, v)
+	}
+}
