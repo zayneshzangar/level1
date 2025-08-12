882244fa88aa90701eaf9e3d941addfa89ba8d3d
zayneshzangar <zangarzaynesh@gmail.com>
2025-08-12 18:08:05 +0500

    Конвейер чисел

---
+++ level1_9/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	arr := []int{1, 2, 3, 4, 5}
+	go func(ch <-chan int) {
+		for n := range ch1 {
+			ch2 <- n * 2
+		}
+		close(ch2)
+	}(ch1)
+
+	go func() {
+		for value := range ch2 {
+			fmt.Println(value)
+		}
+	}()
+
+	for _, value := range arr {
+		ch1 <- value
+	}
+	close(ch1)
+}
