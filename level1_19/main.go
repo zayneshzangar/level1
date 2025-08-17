4c4207ae2a39102218f8f89d68509ee3b3af9885
zayneshzangar <zangarzaynesh@gmail.com>
2025-08-17 16:14:19 +0500

    Разворот строки

---
+++ level1_19/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Reverse переворачивает входную строку, поддерживая Unicode-символы.
+func Reverse(s string) string {
+	runes := []rune(s)
+	length := len(runes)
+	for i := 0; i < length/2; i++ {
+		runes[i], runes[length-1-i] = runes[length-1-i], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	input := "главрыба"
+	reversed := Reverse(input)
+	fmt.Printf("Original: %s\nReversed: %s\n", input, reversed)
+
+	input = "hello 🌍"
+	reversed = Reverse(input)
+	fmt.Printf("Original: %s\nReversed: %s\n", input, reversed)
+}
